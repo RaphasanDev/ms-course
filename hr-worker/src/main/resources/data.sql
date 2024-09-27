e09d67ce3bce44ada16c762f5ccaf984e02076cd
Raphael San <=>
2024-09-26 23:45:50 -0300

    Project hr-worker created

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Theo', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Thomas', 300.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Tayler', 250.0);
