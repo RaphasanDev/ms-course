bacab014f44f4e058095611852aa3b1b9a99fdfc
Raphael San <=>
2024-09-27 23:40:37 -0300

    hr-payroll implementation mock

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperior.hr_payroll;
+package com.devsuperior.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
