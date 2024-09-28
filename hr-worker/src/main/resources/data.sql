439b2a3172f723646cc1d50471879e1223833c45
Raphael San <=>
2024-09-28 00:15:50 -0300

    RestTemplate

--- hr-worker/src/main/resources/data.sql
+++ hr-worker/src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Theo', 200.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Thomas', 300.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Tayler', 250.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Jaque', 350.0);
